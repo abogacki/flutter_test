01a11c3f4948441496495a2f840a749a3a8745cb
abogacki <andrzej.bogacki@intive.com>
2019-09-17 15:11:04 +0200

    Completes step 5 of part 5 of base tutorial

---
+++ lib/randomWords/state.dart
@@ -0,0 +1,41 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggetsions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(pair.asPascalCase, style: _biggerFont),
+      trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildSuggetsions();
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
